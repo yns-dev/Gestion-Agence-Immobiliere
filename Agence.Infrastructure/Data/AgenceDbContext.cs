03a6f73aaba9604fa73e00a9993c10191eb385d2
younes.ettazarini@gmail.com <b2Sn79p39Guewj@>
2025-03-11 09:28:14 +0000

    ajout du db context

---
+++ Agence.Infrastructure/Data/AgenceDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agence.Domain.Models;
+using Agence.Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agence.Infrastructure.Data
+{
+    public class AgenceDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Visite> Visites { get; set; }
+        public DbSet<BienImmobilier> Biens { get; set; }
+        public AgenceDbContext(DbContextOptions<AgenceDbContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new EntityClientConfig());
+            modelBuilder.ApplyConfiguration(new EntityBienConfig());
+            modelBuilder.ApplyConfiguration(new EntityVisiteConfig());
+        }
+    }
+}
